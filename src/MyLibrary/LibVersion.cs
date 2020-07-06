5cd25682aa6732875323683f61b4169222721564
Benito Palacios Sanchez <benito356@gmail.com>
2020-07-06 08:13:31 +0200

    :tada: After split repositories from PleOps

---
+++ src/MyLibrary/LibVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLibrary
+{
+    using System.Reflection;
+
+    public static class LibVersion
+    {
+        public static string GetVersion()
+        {
+            Assembly library = typeof(LibVersion).Assembly;
+            return library.GetName().Version.ToString();
+        }
+    }
+}
